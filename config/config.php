8c795592c3502df2e93379ee998d75fc0087c25e
Quentin Avrain <quentin.avrain@gmail.com>
2024-05-21 15:26:30 +0200

    TOM-65_localDBConfig

--- config/config.php
+++ config/config.php
@@ -1,2 +1,8 @@
 <?php
 
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_NAME', getenv('DB_NAME'));
+
+define('DB_HOST', 'mariadb');
+define('DB_PORT', '3306');
