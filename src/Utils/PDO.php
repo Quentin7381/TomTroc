2c5a4d9bda6f7cdd9422d1b00f735dbb8fcc6d56
Quentin Avrain <quentin.avrain@gmail.com>
2024-06-04 15:53:49 +0200

    TOM-111 Singleton PDO

--- src/Utils/PDO.php
+++ src/Utils/PDO.php
@@ -5,14 +5,23 @@ namespace Utils;
 use Config\Config;
 
 class PDO extends \PDO{
+    protected static $instance;
 
-    public function __construct(){
+    protected function __construct(){
+        self::$instance = $this;
         $config = Config::getInstance();
         parent::__construct(
             'mysql:host=' . $config->db['host'] . ';port=' . $config->db['port'] . ';dbname=' . $config->db['name'],
             $config->db['user'],
             $config->db['password']
         );
+    }
+
+    public static function getInstance(){
+        if(!self::$instance){
+            self::$instance = new self();
+        }
+        return self::$instance;
     }
 
 }
